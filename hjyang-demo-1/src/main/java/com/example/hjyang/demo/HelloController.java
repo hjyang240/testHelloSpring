7c2c2548f564c9cc468b6c8009aa8072c4dfc08b
hjyang240 <hjyang.exem@gmail.com>
2025-10-17 16:34:50 +0900

    start

---
+++ hjyang-demo-1/src/main/java/com/example/hjyang/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.hjyang.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot on OKD!!!";
+    }
+}
---
+++ hjyang-demo-1/src/test/java/com/example/hjyang/demo/HjyangDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hjyang.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjyangDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
